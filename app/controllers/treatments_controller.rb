1c725fdbf6031d180c869c030a58abf7fc64162e
tomoka <tomoka.saiwaki@gmail.com>
2025-11-01 22:44:29 +0900

    治療履歴のscaffold追加後のrubocop指摘事項を修正

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -1,31 +1,26 @@
-class TreatmentsController < ApplicationController
-  before_action :set_treatment, only: %i[ show edit update destroy ]
+# frozen_string_literal: true
 
-  # GET /treatments or /treatments.json
+class TreatmentsController < ApplicationController
+  before_action :set_treatment, only: %i[show edit update destroy]
+
   def index
     @treatments = Treatment.all
   end
 
-  # GET /treatments/1 or /treatments/1.json
-  def show
-  end
+  def show; end
 
-  # GET /treatments/new
   def new
     @treatment = Treatment.new
   end
 
-  # GET /treatments/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /treatments or /treatments.json
   def create
     @treatment = Treatment.new(treatment_params)
 
     respond_to do |format|
       if @treatment.save
-        format.html { redirect_to @treatment, notice: "Treatment was successfully created." }
+        format.html { redirect_to @treatment, notice: 'Treatment was successfully created.' }
         format.json { render :show, status: :created, location: @treatment }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,11 +29,10 @@ class TreatmentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /treatments/1 or /treatments/1.json
   def update
     respond_to do |format|
       if @treatment.update(treatment_params)
-        format.html { redirect_to @treatment, notice: "Treatment was successfully updated.", status: :see_other }
+        format.html { redirect_to @treatment, notice: 'Treatment was successfully updated.', status: :see_other }
         format.json { render :show, status: :ok, location: @treatment }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,24 +41,22 @@ class TreatmentsController < ApplicationController
     end
   end
 
-  # DELETE /treatments/1 or /treatments/1.json
   def destroy
     @treatment.destroy!
 
     respond_to do |format|
-      format.html { redirect_to treatments_path, notice: "Treatment was successfully destroyed.", status: :see_other }
+      format.html { redirect_to treatments_path, notice: 'Treatment was successfully destroyed.', status: :see_other }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_treatment
-      @treatment = Treatment.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def treatment_params
-      params.require(:treatment).permit(:treated_on, :description)
-    end
+  def set_treatment
+    @treatment = Treatment.find(params[:id])
+  end
+
+  def treatment_params
+    params.require(:treatment).permit(:treated_on, :description)
+  end
 end
--- test/models/treatment_test.rb
+++ test/models/treatment_test.rb
@@ -1,7 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class TreatmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
