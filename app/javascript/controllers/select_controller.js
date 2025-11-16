8472201cb27b948c4757596ca9741c334fabf6fb
tomoka <tomoka.saiwaki@gmail.com>
2025-11-16 18:08:28 +0900

    Tom SelectのCSSを修正

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -3,8 +3,13 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="select"
 export default class extends Controller {
   connect() {
-    new TomSelect(this.element, {
+    this.ts = new TomSelect(this.element, {
       create: true,
     });
+
+    const wrapper = this.ts.wrapper;
+    if (this.element.dataset.error === "true") {
+      wrapper.classList.add("error");
+    }
   }
 }
