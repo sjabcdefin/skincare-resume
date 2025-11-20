787cfddda8445349795ce650bb6452d1090ed453
tomoka <tomoka.saiwaki@gmail.com>
2025-11-20 21:57:38 +0900

    入力の確認ページを作成

--- app/controllers/confirms_controller.rb
+++ app/controllers/confirms_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class ConfirmsController < ApplicationController
-  def show; end
+  def show
+    @products = Product.all.order(:started_on)
+    @medications = Medication.all.order(:started_on)
+    @allergies = Allergy.all
+    @treatments = Treatment.all.order(:treated_on)
+  end
 end
