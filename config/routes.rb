77d60961d6906ad6be414622db6127593cc2d45b
tomoka <tomoka.saiwaki@gmail.com>
2025-11-27 20:11:38 +0900

    OmniAuthの設定(ルーティングの設定)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
 
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  post 'logout', to: 'sessions#destroy', as: 'logout'
+
   get "home/index"
   get "confirmation", to: "confirmation#show"
   resources :allergies
