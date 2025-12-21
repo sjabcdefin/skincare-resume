33733fac5e594dddc6a6336e961c720cdeb77809
tomoka <tomoka.saiwaki@gmail.com>
2025-12-21 14:04:27 +0900

    未ログインのときの保存機能を実装(治療履歴ページ)

---
+++ app/models/session_treatment.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class SessionTreatment
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attribute :id, :string
+  attribute :description, :string
+  attribute :treated_on, :date
+  attribute :persisted, :boolean, default: false
+
+  validates :description, presence: true
+
+  def initialize(attributes = {})
+    super(attributes)
+    self.id ||= SecureRandom.uuid
+  end
+
+  def self.model_name
+    Treatment.model_name
+  end
+
+  def persisted?
+    persisted
+  end
+end
