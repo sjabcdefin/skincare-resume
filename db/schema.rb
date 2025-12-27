997b6cea24cdab0a192eaa206c60b6a8902256bb
tomoka <tomoka.saiwaki@gmail.com>
2025-12-27 19:22:04 +0900

    Treatmentsテーブルのdescriptionカラム名をnameに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_11_084108) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_27_102049) do
   create_table "allergies", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_11_084108) do
 
   create_table "treatments", force: :cascade do |t|
     t.date "treated_on"
-    t.string "description"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "skincare_resume_id", null: false
