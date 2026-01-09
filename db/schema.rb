d008d4a693505beeada23289313c763d38ae1f32
tomoka <tomoka.saiwaki@gmail.com>
2026-01-09 18:49:23 +0900

    migration ファイルを適用

--- db/schema.rb
+++ db/schema.rb
@@ -11,4 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2026_01_09_091644) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
 end
