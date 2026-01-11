09eaf77bfc9ab5db2d1ebc37cde02a93e2ea868d
tomoka <tomoka.saiwaki@gmail.com>
2026-01-11 22:21:16 +0900

    ログイン、ログアウト処理を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "products#index"
+
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   post 'logout', to: 'sessions#destroy', as: 'logout'
