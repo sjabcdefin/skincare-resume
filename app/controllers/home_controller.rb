39d88b408d66e68d371327e7603975610e6870fa
tomoka <tomoka.saiwaki@gmail.com>
2026-01-18 21:34:28 +0900

    ログイン前のトップページを作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
+    @resume_status = 'before_login'
+  end
+
+  private
+
+  def decide_resume_state
+    return 'before_login' unless current_user
   end
 end
