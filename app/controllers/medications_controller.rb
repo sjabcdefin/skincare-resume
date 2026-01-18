a16ce449608e1ebe92c9eb74b13b1b9db70d3570
tomoka <tomoka.saiwaki@gmail.com>
2026-01-18 16:52:07 +0900

    未ログイン時の薬の登録機能（index / create）を実装

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -4,8 +4,7 @@ class MedicationsController < ApplicationController
   before_action :set_medication, only: %i[show edit update destroy]
 
   def index
-    resume = current_user&.skincare_resume
-    @medications = resume ? resume.medications.order(:started_on) : []
+    @medications = current_user ? all_for_login : all_for_guest
   end
 
   def show; end
@@ -17,14 +16,11 @@ class MedicationsController < ApplicationController
   def edit; end
 
   def create
-    resume = current_user.skincare_resume
-    resume ||= current_user.create_skincare_resume(status: :draft)
-    @medication = resume.medications.new(medication_params)
+    @medication = current_user ? build_for_login : build_for_guest
 
     if @medication.save
       flash.now.notice = '薬の登録に成功しました。'
     else
-      Rails.logger.info @medication.errors.full_messages
       render :new, status: :unprocessable_entity
     end
   end
@@ -50,5 +46,34 @@ class MedicationsController < ApplicationController
 
   def medication_params
     params.require(:medication).permit(:started_on, :name)
+  end
+
+  def all_for_login
+    resume = current_user&.skincare_resume
+    resume ? resume.medications.order(:started_on) : []
+  end
+
+  def all_for_guest
+    current_resume ? current_resume.medications.order(:started_on) : []
+  end
+
+  def build_for_login
+    resume = current_user.skincare_resume
+    resume ||= current_user.create_skincare_resume(status: :draft)
+    resume.medications.new(medication_params)
+  end
+
+  def build_for_guest
+    resume = current_resume
+    resume ||= SkincareResume.create!(uuid: SecureRandom.uuid, status: :draft, user_id: nil)
+    @session['resume_uuid'] = resume.uuid
+    resume.medications.new(medication_params)
+  end
+
+  def current_resume
+    @session = session
+    return nil unless @session['resume_uuid']
+
+    SkincareResume.find_by(uuid: @session['resume_uuid'])
   end
 end
