8c2649db626d083d7dce2cc6beda825aa456b958
tomoka <tomoka.saiwaki@gmail.com>
2026-01-18 16:54:37 +0900

    薬の新規作成処理の new を build に変更

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -68,14 +68,14 @@ class MedicationsController < ApplicationController
   def build_for_login
     resume = current_user.skincare_resume
     resume ||= current_user.create_skincare_resume(status: :draft)
-    resume.medications.new(medication_params)
+    resume.medications.build(medication_params)
   end
 
   def build_for_guest
     resume = current_resume
     resume ||= SkincareResume.create!(uuid: SecureRandom.uuid, status: :draft, user_id: nil)
     @session['resume_uuid'] = resume.uuid
-    resume.medications.new(medication_params)
+    resume.medications.build(medication_params)
   end
 
   def current_resume
