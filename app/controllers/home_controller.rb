99a4225e78822bc25ee5dd2ba92748b5e5fd124a
tomoka <tomoka.saiwaki@gmail.com>
2026-01-18 21:43:49 +0900

    ログイン後・履歴書作成前のトップページを作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,14 @@
 
 class HomeController < ApplicationController
   def index
-    @resume_status = 'before_login'
+    @resume = current_user&.skincare_resume
+    @resume_status = decide_resume_state
   end
 
   private
 
   def decide_resume_state
     return 'before_login' unless current_user
+    return 'no_resume' unless @resume
   end
 end
