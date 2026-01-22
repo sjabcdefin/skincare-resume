d1fa4bbc421a553bb524aade97ae230c8e73d70d
tomoka <tomoka.saiwaki@gmail.com>
2026-01-22 21:44:56 +0900

    OmniAuth Google ログインが本番で失敗する問題を修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,15 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if ENV["KAMAL_BUILD"] == "true"
-    provider :google_oauth2,
-      "dummy",
-      "dummy",
-      prompt: 'select_account'
-  else
+  if Rails.env.development? || Rails.env.test?
     provider :google_oauth2,
       Rails.application.credentials.google[:client_id],
       Rails.application.credentials.google[:client_secret],
       prompt: 'select_account'
+  elsif ENV["OMNIAUTH_CLIENT_ID"].present? && ENV["OMNIAUTH_CLIENT_SECRET"].present?
+    provider :google_oauth2,
+      ENV.fetch("OMNIAUTH_CLIENT_ID"),
+      ENV.fetch("OMNIAUTH_CLIENT_SECRET"),
+      prompt: 'select_account'
+  else
+    provider :google_oauth2, "dummy", "dummy", prompt: 'select_account'
   end
-
 end
 OmniAuth.config.allowed_request_methods = %i[get]
