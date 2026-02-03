812c7fa0541371e2f32bbeef14ec37258d2d5744
tomoka <tomoka.saiwaki@gmail.com>
2026-02-03 22:03:08 +0900

    ResumeBasedRepositoryを導入し、各Repositoryの共通処理を整理

--- app/repositories/allergies_repository.rb
+++ app/repositories/allergies_repository.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
-class AllergiesRepository
-  def initialize(user:, session:)
-    @user = user
-    @session = session
-  end
-
+class AllergiesRepository < ResumeBasedRepository
   def all
     current_resume&.allergies || Allergy.none
   end
@@ -16,43 +11,5 @@ class AllergiesRepository
 
   def build(params)
     writable_resume.allergies.build(params)
-  end
-
-  private
-
-  def current_resume
-    @current_resume ||= @user ? login_resume : guest_resume
-  end
-
-  def login_resume
-    @user.skincare_resume
-  end
-
-  def guest_resume
-    return nil unless @session['resume_uuid']
-
-    SkincareResume.find_by(uuid: @session['resume_uuid'])
-  end
-
-  def writable_resume
-    @writable_resume ||= @user ? ensure_login_resume : ensure_guest_resume
-  end
-
-  def ensure_login_resume
-    return login_resume if login_resume
-
-    @user.create_skincare_resume(status: :draft)
-  end
-
-  def ensure_guest_resume
-    return guest_resume if guest_resume
-
-    resume = SkincareResume.create!(
-      uuid: SecureRandom.uuid,
-      status: :draft,
-      user_id: nil
-    )
-    @session['resume_uuid'] = resume.uuid
-    resume
   end
 end
--- app/repositories/products_repository.rb
+++ app/repositories/products_repository.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
-class ProductsRepository
-  def initialize(user:, session:)
-    @user = user
-    @session = session
-  end
-
+class ProductsRepository < ResumeBasedRepository
   def all
     current_resume&.products&.order(:started_on) || Product.none
   end
@@ -16,43 +11,5 @@ class ProductsRepository
 
   def build(params)
     writable_resume.products.build(params)
-  end
-
-  private
-
-  def current_resume
-    @current_resume ||= @user ? login_resume : guest_resume
-  end
-
-  def login_resume
-    @user.skincare_resume
-  end
-
-  def guest_resume
-    return nil unless @session['resume_uuid']
-
-    SkincareResume.find_by(uuid: @session['resume_uuid'])
-  end
-
-  def writable_resume
-    @writable_resume ||= @user ? ensure_login_resume : ensure_guest_resume
-  end
-
-  def ensure_login_resume
-    return login_resume if login_resume
-
-    @user.create_skincare_resume(status: :draft)
-  end
-
-  def ensure_guest_resume
-    return guest_resume if guest_resume
-
-    resume = SkincareResume.create!(
-      uuid: SecureRandom.uuid,
-      status: :draft,
-      user_id: nil
-    )
-    @session['resume_uuid'] = resume.uuid
-    resume
   end
 end
