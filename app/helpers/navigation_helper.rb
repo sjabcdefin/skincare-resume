0c81ded4c1663b9c1a3981203a9ae28663982f97
tomoka <tomoka.saiwaki@gmail.com>
2026-02-13 18:41:31 +0900

    入力ページ共通の「前へ」「次へ」ボタンのスタイルを調整

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -3,7 +3,7 @@
 module NavigationHelper
   def nav_button(label, path)
     if path.present?
-      link_to label, path, class: 'w-24 rounded-md text-center px-3.5 py-2 bg-[#5F7F67] hover:bg-[#4A6652] text-white font-medium'
+      link_to label, path, class: 'inline-flex items-center justify-center h-10 w-24 rounded bg-[#5F7F67] hover:bg-[#4A6652] text-sm font-normal text-white'
     else
       content_tag(:span, '', class: 'w-24')
     end
