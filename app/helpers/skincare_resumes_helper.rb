a6c14cc7e650770f0766eda616faef84876d2f1d
tomoka <tomoka.saiwaki@gmail.com>
2026-02-24 14:50:52 +0900

    _resume.html.slim の表示用・印刷用レイアウトを統一

--- app/helpers/skincare_resumes_helper.rb
+++ app/helpers/skincare_resumes_helper.rb
@@ -5,11 +5,23 @@ module SkincareResumesHelper
     resume.status == 'draft' ? '作成途中の履歴書を編集・更新する' : '作成した履歴書を編集・更新する'
   end
 
-  def display_date(date)
+  def print_date(name, date)
+    name ? format_date(date) : blank_cell
+  end
+
+  def print_name(name)
+    name || blank_cell
+  end
+
+  def format_date(date)
     date ? l(date, format: :default) : '－'
   end
 
   def blank_cell
     content_tag(:span, '&nbsp;'.html_safe)
+  end
+
+  def resume_items_empty(resume)
+    resume.products.empty? && resume.medications.empty? && resume.allergies.empty? && resume.treatments.empty?
   end
 end
