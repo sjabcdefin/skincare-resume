7a4901d894a46647f9437c6ad80fee79af17a02c
tomoka <tomoka.saiwaki@gmail.com>
2026-02-28 21:17:47 +0900

    StaticPagesコントローラを作成し、利用規約・プライバシーポリシー表示用ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   resource :skincare_resume do
     get :confirmation
   end
+
+  get "static_pages/terms"
+  get "static_pages/privacy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
