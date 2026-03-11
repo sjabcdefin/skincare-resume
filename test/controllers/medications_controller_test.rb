2bf35bf3a3dc2e3f8630a502d0fc39df5ab17238
tomoka <tomoka.saiwaki@gmail.com>
2026-03-11 22:45:30 +0900

    未ログイン時の薬のコントローラーテストを追加

--- test/controllers/medications_controller_test.rb
+++ test/controllers/medications_controller_test.rb
@@ -5,6 +5,7 @@ require 'test_helper'
 class MedicationsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:alice)
+    @session = { 'resume_uuid' => skincare_resumes(:one).uuid }
     @medication = medications(:one)
   end
 
@@ -69,6 +70,90 @@ class MedicationsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should destroy medication when logged in' do
     sign_in @user do
+      assert_difference('Medication.count', -1) do
+        delete medication_url(@medication), as: :turbo_stream
+      end
+      assert_response :success
+      assert_equal 'text/vnd.turbo-stream.html', @response.media_type
+    end
+  end
+
+  test 'should get index when not logged in' do
+    with_session @session do
+      get medications_url
+      assert_response :success
+    end
+  end
+
+  test 'should get new when not logged in' do
+    with_session @session do
+      get new_medication_url
+      assert_response :success
+    end
+  end
+
+  test 'should create medication when not logged in without session' do
+    assert_difference('Medication.count') do
+      post medications_url,
+           params: {
+             medication: {
+               name: 'ベピオゲル',
+               started_on: Date.new(2025, 12, 25)
+             }
+           },
+           as: :turbo_stream
+    end
+    assert_response :success
+    assert_equal 'text/vnd.turbo-stream.html', @response.media_type
+  end
+
+  test 'should create medication when not logged in' do
+    with_session @session do
+      assert_difference('Medication.count') do
+        post medications_url,
+             params: {
+               medication: {
+                 name: 'ベピオゲル',
+                 started_on: Date.new(2025, 12, 25)
+               }
+             },
+             as: :turbo_stream
+      end
+      assert_response :success
+      assert_equal 'text/vnd.turbo-stream.html', @response.media_type
+    end
+  end
+
+  test 'should show medication when not logged in' do
+    with_session @session do
+      get medication_url(@medication)
+      assert_response :success
+    end
+  end
+
+  test 'should get edit when not logged in' do
+    with_session @session do
+      get edit_medication_url(@medication)
+      assert_response :success
+    end
+  end
+
+  test 'should update medication when not logged in' do
+    with_session @session do
+      patch medication_url(@medication),
+            params: {
+              medication: {
+                started_on: Date.new(2025, 12, 24)
+              }
+            },
+            as: :turbo_stream
+      assert_response :success
+      assert_equal 'text/vnd.turbo-stream.html', @response.media_type
+    end
+  end
+
+  test 'should destroy medication when not logged in' do
+    with_session @session do
       assert_difference('Medication.count', -1) do
         delete medication_url(@medication), as: :turbo_stream
       end
