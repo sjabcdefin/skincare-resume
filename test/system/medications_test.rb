4d5ffb376a3653a3867a7d91c9f94566f3ddff1c
tomoka <tomoka.saiwaki@gmail.com>
2026-03-18 06:52:41 +0900

    薬のシステムテストを追加

--- test/system/medications_test.rb
+++ test/system/medications_test.rb
@@ -3,45 +3,151 @@
 require 'application_system_test_case'
 
 class MedicationsTest < ApplicationSystemTestCase
-  setup do
-    @medication = medications(:one)
-  end
+  test 'logged in user without resume sees medication form' do
+    login users(:bob)
 
-  test 'visiting the index' do
     visit medications_url
-    assert_selector 'h1', text: 'Medications'
+    assert_selector 'h1', text: '使用している薬'
+    assert_selector '#new_medication'
   end
 
-  test 'should create medication' do
+  test 'logged in user creates medication' do
+    login users(:alice)
+
     visit medications_url
-    click_on 'New medication'
+    find('#add-form-button').click
+    assert_selector '#new_medication'
 
-    fill_in 'Name', with: @medication.name
-    fill_in 'Skincare resume', with: @medication.skincare_resume_id
-    fill_in 'Started on', with: @medication.started_on
-    click_on 'Create Medication'
-
-    assert_text 'Medication was successfully created'
-    click_on 'Back'
+    create_medication(date: '002025/12/25', name: 'ベピオローション')
+    assert_text '2025/12/25'
+    assert_text 'ベピオローション'
   end
 
-  test 'should update Medication' do
-    visit medication_url(@medication)
-    click_on 'Edit this medication', match: :first
+  test 'logged in user updates medication' do
+    login users(:alice)
 
-    fill_in 'Name', with: @medication.name
-    fill_in 'Skincare resume', with: @medication.skincare_resume_id
-    fill_in 'Started on', with: @medication.started_on
-    click_on 'Update Medication'
+    visit medications_url
+    medication = Medication.find_by!(name: 'ベピオゲル')
+    find("##{dom_id(medication)}").click
 
-    assert_text 'Medication was successfully updated'
-    click_on 'Back'
+    within "##{dom_id(medication)}" do
+      fill_in '薬名', with: 'ベピオウォッシュゲル'
+      click_on '更新する'
+      assert_text 'ベピオウォッシュゲル'
+    end
   end
 
-  test 'should destroy Medication' do
-    visit medication_url(@medication)
-    accept_confirm { click_on 'Destroy this medication', match: :first }
+  test 'logged in user destorys medication' do
+    login users(:alice)
 
-    assert_text 'Medication was successfully destroyed'
+    visit medications_url
+    medication = Medication.find_by!(name: 'ベピオゲル')
+    find("##{dom_id(medication)}").click
+
+    within "##{dom_id(medication)}" do
+      accept_confirm '本当に削除しますか？' do
+        find('#delete_button').click
+      end
+    end
+    assert_no_selector "##{dom_id(medication)}"
+  end
+
+  test 'logged in user returns to top page' do
+    login users(:alice)
+
+    visit medications_url
+    click_on 'トップページに戻る'
+
+    assert_current_path root_path
+    assert_selector '#medications-table'
+  end
+
+  test 'guest user without resume sees medication form' do
+    visit medications_url
+    assert_selector 'h1', text: '使用している薬'
+    assert_selector '#new_medication'
+  end
+
+  test 'guest user creates medication' do
+    visit medications_url
+
+    create_medication(date: '002025/12/25', name: 'ベピオローション')
+    assert_text '2025/12/25'
+    assert_text 'ベピオローション'
+  end
+
+  test 'guest user updates medication' do
+    visit medications_url
+
+    create_medication(name: 'ベピオローション')
+    assert_selector '#medications', text: 'ベピオローション'
+
+    medication = Medication.find_by!(name: 'ベピオローション')
+    find("##{dom_id(medication)}").click
+
+    within "##{dom_id(medication)}" do
+      fill_in '薬名', with: 'ベピオウォッシュゲル'
+      click_on '更新する'
+      assert_text 'ベピオウォッシュゲル'
+    end
+  end
+
+  test 'guest user destorys medication' do
+    visit medications_url
+
+    create_medication(name: 'ベピオローション')
+    assert_selector '#medications', text: 'ベピオローション'
+
+    medication = Medication.find_by!(name: 'ベピオローション')
+    find("##{dom_id(medication)}").click
+
+    within "##{dom_id(medication)}" do
+      accept_confirm '本当に削除しますか？' do
+        find('#delete_button').click
+      end
+    end
+    assert_no_selector "##{dom_id(medication)}"
+  end
+
+  test 'guest user returns to top page' do
+    visit medications_url
+
+    create_medication(name: 'ベピオローション')
+    assert_selector '#medications', text: 'ベピオローション'
+
+    click_on 'トップページに戻る'
+
+    assert_current_path root_path
+    assert_text 'Google アカウントでログイン'
+    assert_text 'ログインせずに履歴書を作成'
+  end
+
+  test 'guest user saves skincare resume' do
+    visit medications_url
+
+    create_medication(date: '002025/12/25', name: 'ベピオローション')
+    assert_selector '#medications', text: 'ベピオローション'
+
+    click_on '保存する'
+
+    assert_current_path root_path
+
+    within '#medications-table' do
+      row = find('tbody tr', text: 'ベピオローション')
+      within row do
+        assert_selector 'td:nth-child(1)', text: '2025/12/25'
+        assert_selector 'td:nth-child(2)', text: 'ベピオローション'
+      end
+    end
+  end
+
+  private
+
+  def create_medication(name:, date: nil)
+    within '#new_medication' do
+      fill_in '使用開始日', with: date if date.present?
+      fill_in '薬名', with: name
+      click_on '登録する'
+    end
   end
 end
