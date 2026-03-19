66b4cab8898a875014cad6925716a62e63ea265c
tomoka <tomoka.saiwaki@gmail.com>
2026-03-19 19:12:11 +0900

    トップページのシステムテストを追加

---
+++ test/system/home_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class HomeTest < ApplicationSystemTestCase
+  test 'all user sees top page before login' do
+    visit root_path
+    assert_text 'Google アカウントでログイン'
+    assert_text 'ログインせずに履歴書を作成'
+  end
+
+  test 'logged in user without resume sees top page' do
+    login users(:bob)
+
+    assert_selector 'h1', text: 'トップページ'
+    assert_selector 'h2', text: '履歴書を作成して、'
+    assert_selector 'h2', text: '肌の情報を整理しましょう'
+  end
+
+  test 'logged in user with resume sees resume summary' do
+    login users(:alice)
+
+    assert_selector 'h1', text: 'トップページ'
+    assert_selector 'h2', text: 'スキンケアの履歴書'
+    assert_text '保存した履歴書を編集・更新する'
+  end
+
+  test 'guest user can access resume form from top page' do
+    visit root_path
+    click_on 'ログインせずに履歴書を作成'
+    assert_selector 'h1', text: '使用しているスキンケア製品'
+  end
+end
