180d6b576c7070b603d93d6c483c4b17dab951db
tomoka <tomoka.saiwaki@gmail.com>
2026-04-09 21:56:49 +0900

    定期削除処理にログ出力を追加

--- app/jobs/skincare_resumes_cleanup_job.rb
+++ app/jobs/skincare_resumes_cleanup_job.rb
@@ -4,6 +4,14 @@ class SkincareResumesCleanupJob < ApplicationJob
   queue_as :default
 
   def perform
-    SkincareResume.where(user_id: nil).destroy_all
+    Rails.logger.info '[SkincareResumesCleanupJob] start'
+
+    targets = SkincareResume.where(user_id: nil)
+
+    target_count = targets.count
+    Rails.logger.info "[SkincareResumesCleanupJob] target_count=#{target_count}"
+
+    deleted_count = targets.destroy_all.size
+    Rails.logger.info "[SkincareResumesCleanupJob] finished - deleted_count=#{deleted_count}"
   end
 end
