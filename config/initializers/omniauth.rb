44f5b1ff176dd477a97ce2c22774baedf07620c0
tomoka <tomoka.saiwaki@gmail.com>
2026-04-10 20:13:18 +0900

    OmniAuthの設定をENVに統一

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.test?
-    provider :google_oauth2, "dummy", "dummy", prompt: 'select_account'
-  elsif Rails.env.development?
-    provider :google_oauth2,
-      Rails.application.credentials.google[:client_id],
-      Rails.application.credentials.google[:client_secret],
-      prompt: 'select_account'
-  elsif ENV["OMNIAUTH_CLIENT_ID"].present? && ENV["OMNIAUTH_CLIENT_SECRET"].present?
-    provider :google_oauth2,
-      ENV.fetch("OMNIAUTH_CLIENT_ID"),
-      ENV.fetch("OMNIAUTH_CLIENT_SECRET"),
-      prompt: 'select_account'
+  client_id = ENV["OMNIAUTH_CLIENT_ID"]
+  client_secret = ENV["OMNIAUTH_CLIENT_SECRET"]
+
+  if client_id.present? && client_secret.present?
+    provider :google_oauth2, client_id, client_secret, prompt: 'select_account'
   else
     provider :google_oauth2, "dummy", "dummy", prompt: 'select_account'
   end
