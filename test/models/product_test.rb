f49f09440c711cb1260786669e60b19d597cb0eb
tomoka <tomoka.saiwaki@gmail.com>
2026-04-12 10:08:21 +0900

    Productモデルのテストを改善（並び順の検証を分割し、網羅性と可読性を向上）

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -12,34 +12,73 @@ class ProductTest < ActiveSupport::TestCase
     assert_not product.save
   end
 
+  test 'should not save product with blank name' do
+    product = @resume.products.new(name: '')
+    assert_not product.save
+  end
+
   test 'should save product with name' do
     product = @resume.products.new(name: 'ゼオスキン ハイドレーティングクレンザー')
     assert product.save
   end
 
-  test 'order_for_display sorts correctly' do
-    product1 = products(:zoskin_cleanser)
-    product2 = products(:nivea_cream)
-    product3 = products(:collage_lotion)
-    product4 = products(:collage_milk)
+  test 'order_for_display sorts by started_on asc with nulls first' do
     result = @resume.products.order_for_display.to_a
 
-    assert_equal [product2, product4, product3, product1], result
+    assert_nil result.first.started_on
+  end
+
+  test 'order_for_display sorts by id when started_on is same' do
+    cream = products(:nivea_cream)
+    milk = products(:collage_milk)
+    result = @resume.products.order_for_display.to_a
+
+    assert_equal([cream, milk], result.select { |p| p.started_on.nil? })
+  end
+
+  test 'order_for_display sorts correctly' do
+    cleanser = products(:zoskin_cleanser)
+    cream = products(:nivea_cream)
+    lotion = products(:collage_lotion)
+    milk = products(:collage_milk)
+    result = @resume.products.order_for_display.to_a
+
+    assert_equal [cream, milk, lotion, cleanser], result
+  end
+
+  test 'order_for_print sorts by started_on desc with nulls last' do
+    result = @resume.products.order_for_print.to_a
+
+    assert_nil result.last.started_on
+  end
+
+  test 'order_for_print sorts by id when started_on is same' do
+    cream = products(:nivea_cream)
+    milk = products(:collage_milk)
+    result = @resume.products.order_for_print.to_a
+
+    assert_equal([milk, cream], result.select { |p| p.started_on.nil? })
   end
 
   test 'order_for_print sorts correctly' do
-    product1 = products(:zoskin_cleanser)
-    product2 = products(:nivea_cream)
-    product3 = products(:collage_lotion)
-    product4 = products(:collage_milk)
+    cleanser = products(:zoskin_cleanser)
+    cream = products(:nivea_cream)
+    lotion = products(:collage_lotion)
+    milk = products(:collage_milk)
     result = @resume.products.order_for_print.to_a
 
-    assert_equal [product1, product3, product4, product2], result
+    assert_equal [cleanser, lotion, milk, cream], result
   end
 
-  test 'display_date returns started_on' do
+  test 'display_date returns started_on when present' do
     product = products(:zoskin_cleanser)
 
     assert_equal Date.new(2025, 12, 25), product.display_date
+  end
+
+  test 'display_date returns nil when started_on is nil' do
+    product = products(:nivea_cream)
+
+    assert_nil product.display_date
   end
 end
