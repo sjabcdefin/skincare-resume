4f3c51cd5beff8b94b3cb30ae72fee10f9f0d007
tomoka <tomoka.saiwaki@gmail.com>
2026-04-12 12:12:20 +0900

    SkincareResumeモデルのテストを追加（関連データ削除の検証を分割して実装）

--- test/models/skincare_resume_test.rb
+++ test/models/skincare_resume_test.rb
@@ -2,12 +2,32 @@
 
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  test 'destroying resume destroys products' do
-    resume = skincare_resumes(:resume_with_user)
+class SkincareResumeTest < ActiveSupport::TestCase
+  setup do
+    @resume = skincare_resumes(:resume_with_user)
+  end
 
-    assert_difference('Product.count', -4) do
-      resume.destroy!
+  test 'destroying skincare_resume destroys products' do
+    assert_difference('Product.count', -@resume.products.count) do
+      @resume.destroy!
+    end
+  end
+
+  test 'destroying skincare_resume destroys allergies' do
+    assert_difference('Allergy.count', -@resume.allergies.count) do
+      @resume.destroy!
+    end
+  end
+
+  test 'destroying skincare_resume destroys medications' do
+    assert_difference('Medication.count', -@resume.medications.count) do
+      @resume.destroy!
+    end
+  end
+
+  test 'destroying skincare_resume destroys treatments' do
+    assert_difference('Treatment.count', -@resume.treatments.count) do
+      @resume.destroy!
     end
   end
 end
