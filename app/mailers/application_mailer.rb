970816a2bd4adf3b53b85e4f32b9d802a03e5573
tomoka <tomoka.saiwaki@gmail.com>
2026-04-13 21:08:09 +0900

    AdminMailerTestを追加

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['MAIL_FROM_ADDRESS']
   layout 'mailer'
 end
--- test/mailers/admin_mailer_test.rb
+++ test/mailers/admin_mailer_test.rb
@@ -3,7 +3,20 @@
 require 'test_helper'
 
 class AdminMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'job_failed sends email with error information' do
+    error = StandardError.new('test error')
+
+    ENV['ALERT_EMAIL_ADDRESS'] = 'admin@gmail.com'
+    ENV['MAIL_FROM_ADDRESS'] = 'skincare.resume.contact@gmail.com'
+
+    assert_emails 1 do
+      @email = AdminMailer.job_failed(error).deliver_now
+    end
+
+    assert_equal ['admin@gmail.com'], @email.to
+    assert_equal ['skincare.resume.contact@gmail.com'], @email.from
+    assert_equal '[Alert] Cleanup Job Failed', @email.subject
+    assert_includes @email.text_part.body.to_s, 'test error'
+    assert_includes @email.html_part.body.to_s, 'test error'
+  end
 end
