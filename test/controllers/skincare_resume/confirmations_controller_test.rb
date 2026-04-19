fd74050221ed962f3aaaa2a6a526025413951e46
tomoka <tomoka.saiwaki@gmail.com>
2026-04-19 12:31:09 +0900

    SkincareResume::ConfirmationsControllerTestを作成

--- test/controllers/skincare_resume/confirmations_controller_test.rb
+++ test/controllers/skincare_resume/confirmations_controller_test.rb
@@ -1,8 +1,36 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class SkincareResume::ConfirmationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get skincare_resume_confirmations_show_url
+  setup do
+    @alice = users(:alice)
+    @session = { 'resume_uuid' => skincare_resumes(:resume_without_user).uuid }
+  end
+
+  test 'gets show when logged in with resume' do
+    stub_current_user @alice do
+      get skincare_resume_confirmation_path
+      assert_response :success
+    end
+  end
+
+  test 'gets show when logged in without resume' do
+    stub_current_user @bob do
+      get skincare_resume_confirmation_path
+      assert_response :success
+    end
+  end
+
+  test 'gets show when not logged in with resume' do
+    stub_session @session do
+      get skincare_resume_confirmation_path
+      assert_response :success
+    end
+  end
+
+  test 'gets show when not logged in without resume' do
+    get skincare_resume_confirmation_path
     assert_response :success
   end
 end
